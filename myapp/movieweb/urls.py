a7af8e725e5d2961ae6330250a5040df802c7c85
KimYoungHwan <iam.youngh@gmail.com>
2022-05-01 17:43:39 +0900

    헤더부분 링크 연결, 상세페이지 css반영 수정

--- myapp/movieweb/urls.py
+++ myapp/movieweb/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 
-app_name = "movieweb"
+# app_name = "movieweb"
 
 urlpatterns = [  
     path("", views.index.as_view(), name='index'),
-    path("movie/1", views.movie_detail, name="movie_detail"),
+    # path("movie/1", views.movie_detail, name="movie_detail"),
     path("movie/", views.movieweb.as_view(), name = "movieweb"),
-    # path("movie/<int:pk>/", views.deatil_page, name = "detail_page")
+    path("movie/<int:pk>/", views.detailPage.as_view(), name = "detail_page")
 ]
--- myapp/movieweb/views.py
+++ myapp/movieweb/views.py
@@ -9,8 +9,8 @@ import random
 #     return render(request, 'index.html', netflix)
 
 
-def movie_detail(request):
-    return render(request, "movie_detail/detail_page.html")
+# def movie_detail(request):
+#     return render(request, "movie_detail/detail_page.html")
 
 
 class movieweb(generic.TemplateView):
@@ -25,3 +25,19 @@ class index(generic.TemplateView):
         template_name = 'index.html'
         movie = Movie.objects.filter(netflix=1)
         return render(request, template_name, {"movie":movie})
+
+
+class detailPage(generic.TemplateView):     
+    def __init__(self):
+        self.template_name  = "movie_detail/detail_page.html" 
+
+    def get_queryset(self):     
+        results = Movie.objects.filter(id=self.kwargs['pk'])       
+        results = results.values('id', 'title', 'opening_date', 'just_rating', 'imdb_rating', 'runtime', 'synopsis', 'director', 'actors', 'genre','poster_link',)
+
+        return results
+
+    def get(self, request, *args, **kwargs):        
+        self.content = {"movieDetail":self.get_queryset()}
+
+        return render(request, self.template_name, self.content)
